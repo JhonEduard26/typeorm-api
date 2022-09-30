ad67569d7711b72cc9264b2096766027a03bb33a
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-09-29 22:00:24 -0500

    [ADD] typeorm config

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import 'reflect-metadata'
+import app from './app'
+import { AppDataSource } from './db'
+
+
+const main = async () => {
+  try {
+    const port = 3000
+
+    await AppDataSource.initialize()
+    console.log('Database connected!')
+    app.listen(port, () => {
+      console.log(`Listening on port ${port}`)
+    })
+
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+main()

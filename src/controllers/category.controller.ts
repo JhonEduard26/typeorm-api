f6968a73522a6089213d746e380b9dfcfd5f11ab
Jhon Eduard Bocanegra <jhon.bocanegra26@gmail.com>
2022-09-30 09:52:36 -0500

    [ADD] crud categories

--- src/controllers/category.controller.ts
+++ src/controllers/category.controller.ts
@@ -1,11 +1,24 @@
 import { Request, Response } from 'express'
 import { Category } from '../models/category.model'
 
-export const getAllCategories = (req: Request, res: Response) => {
-  return res.json({
-    ok: true,
-    message: 'All categories',
-  })
+export const getAllCategories = async (req: Request, res: Response) => {
+
+  try {
+    const categories = await Category.find()
+    return res.json({
+      ok: true,
+      message: categories,
+    })
+
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({
+        ok: false,
+        message: error.message,
+      })
+    }
+  }
+
 }
 
 export const createCategory = async (req: Request, res: Response) => {
@@ -27,6 +40,67 @@ export const createCategory = async (req: Request, res: Response) => {
       return res.status(500).json({
         ok: false,
         error: error.message
+      })
+    }
+  }
+}
+
+export const updateCategory = async (req: Request, res: Response) => {
+  try {
+    const { category_name, description } = req.body
+    const { id } = req.params
+    const category = await Category.findOneBy({
+      category_id: Number(id),
+    })
+
+    if (category) {
+      category.category_name = category_name
+      category.description = description
+      await Category.save(category)
+      return res.json({
+        ok: true,
+        message: category,
+      })
+    } else {
+      return res.status(404).json({
+        ok: false,
+        message: 'Category doesn\'t exists',
+      })
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({
+        ok: false,
+        message: error.message,
+      })
+    }
+  }
+}
+
+export const deleteCategory = async (req: Request, res: Response) => {
+  try {
+    const { id } = req.params
+    const category = await Category.findOneBy({
+      category_id: Number(id),
+    })
+
+    if (category) {
+      await Category.remove(category)
+      return res.json({
+        ok: true,
+        message: category,
+      })
+    } else {
+      return res.status(404).json({
+        ok: false,
+        message: 'Category doesn\'t exists',
+      })
+    }
+  } catch (error) {
+    if (error instanceof Error) {
+      return res.status(500).json({
+        ok: false,
+        message: error.message,
       })
     }
   }
--- src/routes/category.route.ts
+++ src/routes/category.route.ts
@@ -1,10 +1,14 @@
 import { Router } from 'express'
-import { createCategory, getAllCategories } from '../controllers/category.controller'
+import { createCategory, deleteCategory, getAllCategories, updateCategory } from '../controllers/category.controller'
 
 const router = Router()
 
 router.get('/categories', getAllCategories)
 
 router.post('/categories', createCategory)
+
+router.put('/categories/:id', updateCategory)
+
+router.delete('/categories/:id', deleteCategory)
 
 export default router
